af0710017c7c58a658b1f5c44246102859b71dac
Davi-Fran <dafaosi.avlis@gmail.com>
2026-05-25 11:05:46 -0300

    Get products from CSV and generate excel report: LOPAL

--- 1-semestre/LOPAL/main.py
+++ 1-semestre/LOPAL/main.py
@@ -1,5 +1,15 @@
-def main():
-    ...
+from model.products import Products
+from service.excel_report import Excel
+
+def app():
+    try:
+        products = Products()
+
+        # Recebe os produtos no formato de dataframe e gera um relatório em .xlsx
+        report_excel = Excel(products.get_dataframe())
+        report_excel.generate_report('Relatório')
+    except FileNotFoundError:
+        print('Base de dados não encontrada!')
 
 if __name__ == '__main__':
-    main()
+    app()

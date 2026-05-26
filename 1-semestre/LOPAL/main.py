55369ad634a9a3cd1790dd1e9968dc823917ae38
Davi-Fran <dafaosi.avlis@gmail.com>
2026-05-26 11:16:59 -0300

    Gen HTML page service done

--- 1-semestre/LOPAL/main.py
+++ 1-semestre/LOPAL/main.py
@@ -5,12 +5,17 @@ from view.inventory_view import InventoryPage
 def app():
     try:
         products = Products()
+        df = products.get_dataframe()
+        print(df)
 
         # Recebe os produtos no formato de dataframe e gera um relatório em .xlsx
-        report_excel = Excel(products.get_dataframe())
-        report_excel.generate_report('Relatório')
+        """ report_excel = Excel(products.get_dataframe())
+        report_excel.generate_report('Relatório') """
+
+        idk = InventoryPage(df)
+        idk.generate_page()
     except FileNotFoundError:
-        print('Base de dados não encontrada!')
+        print('Base não encontrada!')
 
 if __name__ == '__main__':
     app()
--- 1-semestre/LOPAL/view/inventory_view.py
+++ 1-semestre/LOPAL/view/inventory_view.py
@@ -1,13 +1,12 @@
+from pandas import DataFrame
+import os
+
 class InventoryPage:
-    def __init__(self):
-        self.html_products_elements = """"""
-        self.html_template_page = """<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estoque Relatório</title>
-    <style>
+    def __init__(self, dataframe: DataFrame):
+        self.dataframe = dataframe
+        self.html_template_page = ''
+        self.html_products_elements = ''
+        self.css_styles = """<style>
         * {
             margin: 0;
             padding: 0;
@@ -18,26 +17,84 @@ class InventoryPage:
         body {
             height: 100vh;
             width: 100vw;
+            overflow-x: hidden;
+        }
+
+        header {
+            width: 100%;
+            height: 12%;
             display: flex;
-            flex-direction: column;
             justify-content: center;
             align-items: center;
+            box-shadow: 0 0 5px #000;
         }
 
         h1 {
             font-size: 2.5rem;
         }
-    </style>
+
+        main {
+            width: 50%;
+            display: block;
+            margin: 0 auto;
+            margin-top: 2rem;
+        }
+
+        article {
+
+            margin: 0.5rem;
+            border: 1px solid #000;
+            padding: 1rem;
+            border-radius: 5px;
+        }
+
+        h2 {
+            margin-bottom: 0.3rem;
+        }
+
+        p {
+            margin: 0.3rem 0;
+        }
+    </style>"""
+
+    def generate_page(self, page_name='inventory_page.html'):
+        desktop_path = f'{os.getcwd()}/../../../'
+
+        for i in range(len(self.dataframe)):
+            qtde_produto_disponivel = self.dataframe.iloc[i]['Qtde_Disponivel']
+            qtde_produto_reservado = self.dataframe.iloc[i]['Qtde_Reservada']
+            total_produto_estoque = qtde_produto_disponivel + qtde_produto_reservado
+
+            html_product_template = f"""<article>
+            <h2>{self.dataframe.iloc[i]['Produto']}</h2>
+            <hr>
+            <p>Código: <strong>{self.dataframe.iloc[i]['Codigo']}</strong></p>
+            <p>Quantidade disponível: <strong>{qtde_produto_disponivel}</strong></p>
+            <p>Quantidade reservada: <strong>{qtde_produto_reservado}</strong></p>
+            <p>Total em estoque: <strong>{total_produto_estoque}</strong></p>
+            <p>Preço Unitário: <strong>R${self.dataframe.iloc[i]['Preco']}</strong></p>
+         </article>"""
+
+            self.html_products_elements += html_product_template
+
+        self.html_template_page = f"""<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estoque Relatório</title>
+    {self.css_styles}
 </head>
 <body>
     <header>
         <h1>Relatório de produtos em estoque</h1>
     </header>
+
+    <main>
+        {self.html_products_elements}
+    </main>
 </body>
 </html>"""
 
-        
-
-    def generate_page(self, page_name='inventory_page.html'):
-        with open(page_name, 'w', encoding='UTF-8') as file:
+        with open(f'{desktop_path}/{page_name}.html', 'w', encoding='UTF-8') as file:
             file.write(self.html_template_page)
